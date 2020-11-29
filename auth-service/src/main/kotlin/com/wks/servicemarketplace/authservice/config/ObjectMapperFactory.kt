4405a68ec50e10feb013c0131908e08f3b80dab1
Waqqas Sheikh <waqqas.abdulkareem@gmail.com>
2020-11-29 12:44:16 +0400

    Consistent configuration for ObjectMapper

--- auth-service/src/main/kotlin/com/wks/servicemarketplace/authservice/config/ObjectMapperFactory.kt
+++ auth-service/src/main/kotlin/com/wks/servicemarketplace/authservice/config/ObjectMapperFactory.kt
@@ -1,7 +1,10 @@
 package com.wks.servicemarketplace.authservice.config
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.glassfish.hk2.api.Factory
 
@@ -9,7 +12,10 @@ class ObjectMapperFactory : Factory<ObjectMapper> {
 
     private val objectMapper: ObjectMapper = ObjectMapper()
             .registerModule(KotlinModule())
+            .registerModule(JavaTimeModule())
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .setSerializationInclusion(JsonInclude.Include.NON_NULL)
 
     override fun provide() = objectMapper
 
--- auth-service/src/main/kotlin/com/wks/servicemarketplace/authservice/config/ObjectMapperProvider.kt
+++ auth-service/src/main/kotlin/com/wks/servicemarketplace/authservice/config/ObjectMapperProvider.kt
@@ -1,6 +1,10 @@
 package com.wks.servicemarketplace.authservice.config
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import javax.ws.rs.Consumes
@@ -17,6 +21,10 @@ class ObjectMapperProvider : JacksonJaxbJsonProvider(objectMapper, DEFAULT_ANNOT
         val objectMapper = ObjectMapper()
         init{
             objectMapper.registerModule(KotlinModule())
+                    .registerModule(JavaTimeModule())
+                    .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                    .setSerializationInclusion(JsonInclude.Include.NON_NULL)
         }
     }
 }
