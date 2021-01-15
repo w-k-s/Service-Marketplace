61a0ae6ba5e673c94ef11cfb17c6e2ef116c6ef1
Waqqas Sheikh <waqqas.abdulkareem@gmail.com>
2021-01-15 18:17:37 +0400

    Configure docker images

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 group = "com.wks.servicemarketplace"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_13
 
 repositories {
 	mavenCentral()
