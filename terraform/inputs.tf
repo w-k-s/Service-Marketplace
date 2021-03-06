4e376928c1ecec717d5e73c7086a341f4995246f
Waqqas Sheikh <waqqas.abdulkareem@gmail.com>
2021-03-06 14:52:28 +0400

    Run terraform apply from github actions

--- terraform/inputs.tf
+++ terraform/inputs.tf
@@ -5,20 +5,20 @@ variable "vpc_id" {
 
 variable "public_subnet_ids" {
   description = "VPC Public Subnet IDs"
-  type = list(string)
+  type        = list(string)
 }
 
 variable "private_subnet_ids" {
   description = "VPC Private Subnet IDs"
-  type = list(string)
+  type        = list(string)
 }
 
 variable "cloudamqp_customer_api_key" {
   description = "CloudAMQP Customer API Key"
-  type = string
+  type        = string
 }
 
 variable "notification_email" {
   description = "Email address where notifications are sent e.g. from CloudAMQP"
-  type = string
+  type        = string
 }
--- terraform/mq.tf
+++ terraform/mq.tf
@@ -1,17 +1,17 @@
 # https://registry.terraform.io/providers/cloudamqp/cloudamqp/latest/docs
 
 provider "cloudamqp" {
-  apikey        = var.cloudamqp_customer_api_key
+  apikey = var.cloudamqp_customer_api_key
 }
 
 # Create a new cloudamqp instance
 resource "cloudamqp_instance" "instance" {
-  name          = "terraform-cloudamqp-instance"
-  plan          = "lemur"
-  region        = "amazon-web-services::region=ap-south-1"
-  nodes         = 1
-  tags          = [ "terraform" ]
-  rmq_version   = "3.8.14"
+  name        = "terraform-cloudamqp-instance"
+  plan        = "lemur"
+  region      = "amazon-web-services::region=ap-south-1"
+  nodes       = 1
+  tags        = ["terraform"]
+  rmq_version = "3.8.14"
 }
 
 # New recipient to receieve notifications
@@ -36,7 +36,7 @@ resource "cloudamqp_alarm" "cpu_alarm" {
 # Cloudwatch logs integration
 resource "cloudamqp_integration_log" "cloudwatchlog" {
   instance_id = cloudamqp_instance.instance.id
-  name = "cloudwatchlog"
+  name        = "cloudwatchlog"
   # access_key_id = var.aws_access_key
   # secret_access_key = var.aws_secret_key
   # region = var.aws_region
@@ -45,7 +45,7 @@ resource "cloudamqp_integration_log" "cloudwatchlog" {
 # Cloudwatch metrics integration
 resource "cloudamqp_integration_metric" "cloudwatch" {
   instance_id = cloudamqp_instance.instance.id
-  name = "cloudwatch"
+  name        = "cloudwatch"
   # access_key_id = var.aws_access_key
   # secret_access_key = var.aws_secret_key
   # region = var.aws_region
